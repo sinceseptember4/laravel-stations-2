c7a0f66c8c7b5b02850e6dd85af99f240d85fa9b
sinceseptember4 <sinceseptember4@gmail.com>
2023-06-01 16:41:55 +0900

    save practice3

---
+++ app/Http/Controllers/PracticeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PracticeController extends Controller
+{
+    public function sample()
+    {
+        return view('practice');
+    }
+
+    public function sample2()
+    {
+        $test = 'practice2';
+            return view('practice2', ['testParam' => $test]);
+    }
+    public function sample3()
+    {
+        $testPram = 'test';
+            return view('practice3', ['testParam' => $testPram]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\PracticeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('URL', [Controllerの名前::class, 'Controller内のfunction名']);
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
